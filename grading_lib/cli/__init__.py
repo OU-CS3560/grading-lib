ae55bedcb1ac3ea5842e9c9047e11748ef1a32af
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2024-08-30 17:15:24 -0400

    feat: add rebase_todo_injector_command

--- grading_lib/cli/__init__.py
+++ grading_lib/cli/__init__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import click
 
 from ..util import get_problem_total_points, load_problems_metadata
@@ -35,6 +37,36 @@ def summary_command() -> None:
         name = problem["problem"]["name"]
         score_section = f"{points:.2f} / {total_points:.2f}"
         print(f" - {name:<40} {score_section:>20}")
+
+
+@cli.command(name="rebase-todo-injector")
+@click.argument("todo_items_file_path")
+@click.argument("path")
+def rebase_todo_injector_command(todo_items_file_path, path) -> None:
+    """
+    An 'editor' that inject pre-made todo items for git interactive rebase.
+
+    It assumes that the pre-made todo items file is named 'rebase-todo-items.txt'
+    and it is in the current working directory.
+
+    This command cannot be used directly, you want to create a script file that call
+    this command.
+
+    Example script file:
+    #!/bin/bash
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    python -m grading_lib rebase-todo-injector $SCRIPT_DIR/../rebase-todo-items.txt $1
+    """
+    todo_items_file_path = Path(todo_items_file_path)
+    if not todo_items_file_path.exists():
+        raise FileNotFoundError(
+            f"Cannot find '{todo_items_file_path}' in the current working directory (cwd='{Path(".").absolute()}')"
+        )
+    with open(todo_items_file_path) as in_f:
+        todo_items_content = in_f.read()
+
+        with open(path, "w") as out_f:
+            out_f.write(todo_items_content)
 
 
 cli.add_command(dev)
